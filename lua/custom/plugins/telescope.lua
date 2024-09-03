3ab7b07ccff8de12dc8816a4b7f4ec4545a74287
Vinz <vinzk28@outlook.com>
2024-09-03 15:14:31 -0400

    Added cder for telescope

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -107,6 +107,61 @@ return {
 
   {
     'zane-/cder.nvim',
-    opts = {},
+    opts = {
+      -- The title of the prompt.
+      prompt_title = function()
+        return 'cwd: ' .. vim.fn.getcwd()
+      end,
+
+      dir_command = { 'fd', '--hidden', '--type=d', '.', os.getenv 'HOME' },
+
+      -- The binary used to execute previewer_command | pager_command.
+      -- This is needed because termopen in Neovim does not support piping
+      -- multiple commands, so we get around this by just using bash -c.
+      command_executer = { 'bash', '-c' },
+
+      -- The command used to preview directories. Defaults to ls.
+      -- Example:
+      --   previewer_command = 'exa -a --icons'
+      previewer_command = 'ls -a',
+
+      -- A function to return an entry given an entry produced
+      -- by dir_command. Returns the entry directly by default.
+      entry_value_fn = function(entry_value)
+        return '"' .. entry_value .. '"'
+      end,
+
+      -- The command used to page directory previews. Defaults to bat.
+      -- Receives the output of the previewer_command as input.
+      -- Example without bat:
+      --   pager_command = 'less -RS'
+      pager_command = 'bat --plain --paging=always --pager="less -RS"',
+
+      -- Function to create an entry in the picker given
+      -- a line outputted from the dir_command.
+      --   value is used as the directory in the :cd command
+      --   display is what is actually displayed, so this can
+      --     be a function that trims a path prefix for example.
+      entry_maker = function(line)
+        return {
+          value = line,
+          display = function(entry)
+            return ' ' .. line:gsub(os.getenv 'HOME' .. '/', ''), { { { 1, 3 }, 'Directory' } }
+          end,
+          ordinal = line,
+        }
+      end,
+
+      -- A mapping should be a function that takes as parameter the selected
+      -- directory as a string.
+      mappings = {
+        default = function(directory)
+          vim.cmd.cd(directory)
+        end,
+        ['<C-t>'] = function(directory)
+          vim.cmd.tcd(directory)
+        end,
+      },
+    },
   },
 }
