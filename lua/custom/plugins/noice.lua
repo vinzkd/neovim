cf82b79425dff703c9cbffab664855324ad7d34d
Vinz <vinzk28@outlook.com>
2024-09-12 09:20:40 -0400

    Moved some plugins to their own .lua file

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -1,11 +1,5 @@
 return {
   {
-    'rcarriga/nvim-notify',
-    opts = {
-      background_colour = '#0D0C12',
-    },
-  },
-  {
     'folke/noice.nvim',
     event = 'VeryLazy',
     opts = {
---
+++ lua/custom/plugins/notify.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    'rcarriga/nvim-notify',
+    opts = {
+      background_colour = '#0D0C12',
+    },
+  },
+}
