3dc39f501a7ebec77003f2a735c970f338d94426
Vinz <vinzk28@outlook.com>
2024-09-11 22:51:01 -0400

    Added noice.nvim

---
+++ lua/custom/plugins/ui.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    'rcarriga/nvim-notify',
+    opts = {
+      background_colour = '#0D0C12',
+    },
+  },
+  {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      'MunifTanjim/nui.nvim',
+    },
+  },
+}
