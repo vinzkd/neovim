32871c14bb37418d5ead56547581662ea0174270
Vinz <vinzk28@outlook.com>
2024-09-24 15:09:58 -0400

    Added hyprls and texlab lsp.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'hyprlang', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'diff', 'hyprlang', 'html', 'latex', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
@@ -17,6 +17,12 @@ return {
       },
       indent = { enable = true, disable = { 'ruby' } },
     },
+
+    -- Hyprland files
+    vim.filetype.add {
+      pattern = { ['.*/hyprland%.conf'] = 'hyprlang' },
+    },
+
     -- There are additional nvim-treesitter modules that you can use to interact
     -- with nvim-treesitter. You should go explore a few and see what interests you:
     --
