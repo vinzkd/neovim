63b17ef650c2688476a85c678362c18d8ffb6429
Vinz <vinzk28@outlook.com>
2024-09-29 23:09:44 -0400

    Switched to indentmini.nvim
    For some reason, indent_blanklines doesn't work well with c++ files

--- lua/plugins/indent_lines.lua
+++ lua/plugins/indent_lines.lua
@@ -1,15 +1,27 @@
 return {
   {
+    'nvimdev/indentmini.nvim',
+    enabled = true,
+    opts = {},
+  },
+
+  {
     'echasnovski/mini.indentscope',
+    enabled = false,
     version = false,
     opts = {
       draw = {
-        delay = 0,
+        delay = 100,
+        priority = 2,
+        animation = function(s, n)
+          return s / n * 20
+        end,
       },
     },
   },
   {
     'lukas-reineke/indent-blankline.nvim',
+    enabled = false,
     main = 'ibl',
     ---@module "ibl"
     ---@type ibl.config
