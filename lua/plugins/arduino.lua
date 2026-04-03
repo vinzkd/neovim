a9f45ee86a46328ee691b0295088dcdbedd2c68a
vinzkd <vinzk28@outlook.com>
2026-04-02 21:43:18 -0400

    Added ArduinoAttach keymap to arduino plugin

--- lua/plugins/arduino.lua
+++ lua/plugins/arduino.lua
@@ -1,17 +1,62 @@
 return {
   {
     'stevearc/vim-arduino',
-    enabled = false,
-  },
+    enabled = true,
+    ft = 'arduino',
 
-  {
-    'vlelo/arduino-helper.nvim',
-    enabled = false,
+    keys = {
+      -- <leader>aa: Attach Board
+      {
+        '<leader>aa',
+        function()
+          vim.cmd 'ArduinoAttach'
+        end,
+        desc = 'Arduino: Attach Board',
+      },
+      -- <leader>au: Upload the sketch
+      {
+        '<leader>au',
+        function()
+          vim.cmd 'ArduinoUpload'
+        end,
+        desc = 'Arduino: Upload Sketch',
+      },
 
-    config = function()
-      require('arduino-helper').setup {
-        ui = 'telescope',
-      }
-    end,
+      -- <leader>as: Open the Serial Monitor
+      {
+        '<leader>as',
+        function()
+          vim.cmd 'ArduinoSerial'
+        end,
+        desc = 'Arduino: Serial Monitor',
+      },
+
+      -- <leader>ac: Compile the sketch
+      {
+        '<leader>ac',
+        function()
+          vim.cmd 'ArduinoCompile'
+        end,
+        desc = 'Arduino: Compile Sketch',
+      },
+
+      -- <leader>ap: Select the Port
+      {
+        '<leader>ap',
+        function()
+          vim.cmd 'ArduinoSelectPort'
+        end,
+        desc = 'Arduino: Select Port',
+      },
+
+      -- <leader>ab: Select the Board
+      {
+        '<leader>ab',
+        function()
+          vim.cmd 'ArduinoSelectBoard'
+        end,
+        desc = 'Arduino: Select Board',
+      },
+    },
   },
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -124,6 +124,7 @@ return {
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
+        arduino_language_server = {},
         clangd = {},
         bashls = {},
         hyprls = {},
